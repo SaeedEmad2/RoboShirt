5a6c56f0f96d965a309952cdf164e03d7cc9e52c
SaeedEmad2 <322223876@sha.edu.eg>
2025-04-14 14:09:12 +0200

    Add template model

--- designs/models.py
+++ designs/models.py
@@ -6,8 +6,26 @@ class Design(models.Model):
     design_description = models.TextField()
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now_add= True)
-    
+
+class Template(models.Model):
+    CATEGORY_CHOICES = [
+        ('movies', 'Movies'),
+        ('music', 'Music'),
+        ('quotes', 'Quotes'),
+        ('family', 'Family'),
+        ('gamers', 'Gamers'),
+        ('sports', 'Sports'),
+        ('funny', 'Funny'),
+        ('spooky', 'Spooky'),
+    ]
+
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+    image = models.ImageField(upload_to='templates/')
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
     
 
 
-# Create your models here.
